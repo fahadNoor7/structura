4bb51b9fba187638d917b2a5324efdec528657f2
fahad.noor <fahad.noor@wsd.com>
2026-06-01 18:21:34 +0200

    Initial scaffold

---
+++ src/main/java/com/wsd/structura/StructuraApplication.java
@@ -0,0 +1,13 @@
+package com.wsd.structura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructuraApplication.class, args);
+	}
+
+}
